a3f2a0d09509260c3424c1795a1a082897056ca2
juniorpen01 <01juniorpen@gmail.com>
2026-01-04 16:07:05 +0800

    Test out type system and variadic templates

--- learn-cpp/main.cpp
+++ learn-cpp/main.cpp
@@ -1,4 +1,22 @@
 #include<iostream>
+#include<tuple>
 
-/// @brief prints "Hello world"
-int main() { std::cout << "Hello world\n"; }
+template<typename...Args>
+auto add(Args... args) {
+	return (args + ...);
+}
+
+template<typename...Args>
+void print(Args... args) {
+	(std::cout << ... << args);
+}
+
+int main() {
+	std::cout << "Hello world\n";
+	auto foo{ std::make_unique<int>(2) };
+	add(1, 2, 3);
+	add(3.0);
+	std::tuple<int, int> bar{ 1,2 };
+	print(1, 2,"foo");
+}
+
